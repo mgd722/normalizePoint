c43c9c486b90b1879b4deef9b18a605fc15701dc
Mike Davlantes <rocks722@hotmail.com>
2015-05-07 18:41:49 -0600

    Create normalizePoint.js

---
+++ normalizePoint.js
@@ -0,0 +1,26 @@
+// Pass point as [lon, lat] array.
+function normalizePoint(point) {
+    var lon = point[0];
+    var lat = point[1];
+    
+    if (Math.abs(lon) >= 180) {
+        while (lon >= 180) {
+            lon -= 360;
+        }
+        while (lon <= -180) {
+            lon += 360;
+        }
+    }
+
+    if (Math.abs(lat) >= 90) {
+        while (lat >= 90) {
+            lat -= 180;
+        }
+        while (lat <= -90) {
+            lat += 180;
+        }
+        lat *= -1;
+    }
+    
+    return [lon, lat];
+}
