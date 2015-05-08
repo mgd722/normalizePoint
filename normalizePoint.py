9c9ecd2403262c75410348b7eae2d5725f104800
Mike Davlantes <rocks722@hotmail.com>
2015-05-07 18:29:20 -0600

    Create normalizePoint.py

---
+++ normalizePoint.py
@@ -0,0 +1,18 @@
+# Pass point as a (lon, lat) tuple.
+def normalizePoint(point):
+	lon, lat = point[0], point[1]
+
+	if abs(lon) >= 180:
+		while lon >= 180:
+			lon -= 360
+		while lon <= -180:
+			lon += 360
+
+	if abs(lat) >= 90:
+		while lat >= 90:
+			lat -= 180
+		while lat <= -90:
+			lat += 180
+		lat *= -1
+
+	return (lon, lat)
